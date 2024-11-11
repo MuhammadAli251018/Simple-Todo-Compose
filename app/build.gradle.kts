3dd61b6c6ca1a933879f6ce3ff682b578473e349
Muhammad Ali <muhammadali251018@gmail.com>
2024-11-11 16:14:01 +0200

    new(dep): Add Serialization & Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+//    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -72,5 +73,8 @@ dependencies {
     implementation(libs.koin.androidx.compose)
     implementation(libs.koin.androidx.compose.navigation)
 
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+//    implementation(libs.kotlinx.serialization.json)
 
 }
